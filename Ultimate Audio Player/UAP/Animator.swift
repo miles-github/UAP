14555b0c93313d3218dbbc0b9d87fb41cdb6ee0a
Adrian Bolinger <adrian.bolinger@me.com>
2016-04-06 22:59:56 -0400

    Added Animation class, added NSNotifications to update UI from AudioPlayer.

---
+++ Ultimate Audio Player/UAP/Animator.swift
@@ -0,0 +1,13 @@
+//
+//  Animator.swift
+//  UAP
+//
+//  Created by Adrian Bolinger on 4/6/16.
+//  Copyright © 2016 NoVA. All rights reserved.
+//
+
+import UIKit
+
+class Animator: NSObject {
+
+}
